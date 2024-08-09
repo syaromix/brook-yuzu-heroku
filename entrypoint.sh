fe607e34acaa8a17c74cc622ec121fbd55c5e21f
syaromix <5601942+syaromix@users.noreply.github.com>
2024-08-09 19:33:38 +0800

    v0.2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## Install brook
-wget -q https://github.com/txthinking/brook/releases/v20240214/download/brook_linux_amd64 -O /usr/local/bin/brook
+wget -q https://github.com/txthinking/brook/releases/v20240404/download/brook_linux_amd64 -O /usr/local/bin/brook
 chmod +x /usr/local/bin/brook
 
 ## Start service
